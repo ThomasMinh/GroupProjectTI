a4e6ebbfa7629da908281fcabe1cb10ebd16f193
Tim Hardeman <57070642+TimHardeman@users.noreply.github.com>
2020-03-27 00:21:54 +0100

    Update roomCheck.cpp

--- roomCheck.cpp
+++ roomCheck.cpp
@@ -1,7 +1,20 @@
-#include <stdio.h>
+#include <vector>
+#include <iostream>
+using namespace std;
 
-int main(int argc, char **argv)
-{
-	printf("hello world\n");
-	return 0;
+vector<int> validRooms (const int & room){
+	vector<vector<int>> rooms =  {{2, 4, 8}, {1, 3, 7}, {2, 4, 6}, {1, 3, 5}, {4, 6, 8}, {3, 5, 7}, {2, 6, 8}, {1, 5, 7}};
+	
+	return rooms[room - 1];
+}
+
+bool isRoomValid(const int & currentRoom, const int & targetRoom){
+	vector<int> rooms = validRooms(currentRoom);
+	
+	for(unsigned int i = 0; i < rooms.size(); i++){
+		if (rooms[i] == targetRoom){
+			return true;
+		}
+	}
+	return false;
 }
