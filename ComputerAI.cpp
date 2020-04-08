2375be4e92c5d1ec63d023f7150acf6dd22f998f
nmcb02 <57104986+nmcb02@users.noreply.github.com>
2020-04-08 14:53:51 +0200

    Update ComputerAI.cpp

--- ComputerAI.cpp
+++ ComputerAI.cpp
@@ -1,21 +1,24 @@
 #include "CoronaFunctions.h"
 
 using namespace std;
-
-void PlacePlayer() {
-    int startingPosition = rand() % 20 + 1; //random nummer wordt gegenereerd tussen 1-20
-    int currentRoom = Move(startingPosition); //speler wordt gemoved naar dat random nummer 
-} 
+void moveComputer(){
+    int roomChoice = 0;
+    int computerRoom = 0;
+    vector<int> neighbourRooms = read(computerRoom); //leest alle buurkamers die er zijn
+    int randNumber = rand() % 3; //genereert een random nummer tussen 0 en 2
+    
+    cout << "Which room? " << endl;
+    roomChoice = neighbourRooms[randNumber]; // kiest een kamer van 1 van de buurkamers
+    cout << roomChoice << endl; //de computer laat zijn keuze zien
+    computerRoom = Move(roomChoice);
+}
 
 void PlayGameComputer(){
     srand (time(NULL));
-    int currentRoom = 0;
-    int roomChoice = 0;
     bool validChoice = false;
     bool computerAlive = true;
     vector<vector<int>> map = generateMap();
-    vector<int> neighborRooms = read(currentRoom); //leest alle buurkamers die er zijn
-    int randNumber = rand() % 3; //genereert een random nummer tussen 0 en 2
+
     
     srand(time(NULL));  // rand() zal hierdoor steeds een andere selectie van getallen genereren
     PlacePlayer();
@@ -30,15 +33,33 @@ void PlayGameComputer(){
         cout << "Enter an action choice." << endl;
         cout << "1) Move" << endl;
         cout << "2) Shoot?" << endl;
-        cout << "3) Quit" << endl;
         cout << ">>> ";
  
         do{
             validChoice = true;
             cout << "Please make a selection: ";
-                roomChoice = neighborRooms[randNumber]; // kiest een kamer van 1 van de buurkamers
-                cout << roomChoice << endl; //laat de computer zijn keuze zien
-                Move(roomChoice);
+            try{
+                int choice = 1;
+                switch(choice){
+                    case 1:
+                    cout << choice << endl;
+                    moveComputer();
+                    break;
+                    
+                    case 2:
+                    cout << "Shooting";
+                    break;
+                    
+                    default:
+                    validChoice = false;
+                    cout << "Invalid choice. Please try again." << endl;
+                    break;
+                }
+            }
+            catch(...){
+                validChoice = false;
+                cout << "Invalid choice. Please try again." << endl;
+            }
         }
         while (validChoice == false);
     }
