a27604ffe1db4c95e87a163f6f31708c72a5f9de
Tim Hardeman <57070642+TimHardeman@users.noreply.github.com>
2020-04-08 18:41:45 +0200

    Add files via upload

---
+++ aiShoot.cpp
@@ -0,0 +1,40 @@
+	// Als de hint dat de corona dichtbij is gegeven is, verhoog het aantal beurten dat de hint gegeven is met 1.
+	if (smellCorona == true){
+		turnsSmelled++;
+	}
+	else{
+		turnsSmelled = 0;
+	}
+	
+	// Als de corona vaker dan 1 keer geroken is en er nog sprays over zijn, spray in 1 van de buurkamers.
+	if (turnsSmelled > 1 && sprays > 0){
+		sprays--;
+		vector<int> neighbours = read(currentRoom);
+		int choice = rand() % 3;
+		
+		// Als het de kamer was met de corona, dan heeft de computer gewonnen.
+		if(neighbours[choice] == coronaRoom){
+			cout << "Splashhhhh!" << endl;
+			cout << "The Corona is dead. You killed it!" << endl;
+			cout << "Congratulations!! You win!!" << endl;
+			coronaAlive = false; // Corona is dood
+			PlayGame();
+		}
+		// Als het niet de goede kamer was beweegt de wumpus.
+		else{
+			cout << "Oh no!!, you missed the Corona!!" << endl;
+			cout << "The Corona flew to another room " << endl;
+			turnsSmelled = 0;
+			moveCorona();
+				// Als de corona naar de kamer dat de computer zat bewoog, dan heeft de computer verloren.
+				if(coronaRoom == currentRoom){
+					cout << "The Corona flew to your room" << endl;
+					cout << "YOU ARE INFECTED, now you're dead. You suck, loser" << endl;
+					PlayAgain();
+				}
+				else{
+					// Als het spel nog niet voorbij is, ga naar de volgende beurt.
+					InspectCurrentRoom();
+				}
+		}
+	}
